6eef2b28f254d152c35cf7dc4a06c1abea343192
christhianrobertnevesramos <christhianrobertnevesramos@gmail.com>
2025-03-31 05:21:36 +0000

    inicio  das estilizações

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import iconStar from "./assets/iconStar.svg"
+import iconStar from "./assets/icon-Star.svg"
 
 export function App() {
   return(
-    <div>
+    <div className="bg-gradient-dark text-white mx-6 p-6">
       <div>
-        <img src={iconStar} alt="iconstar" />
+        <img src={iconStar} alt="" />
       </div>
-
-
-
       <h1>How did we do?</h1>
 
       <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
       <div>
-        <input type="button" value={1} />
-        <input type="button" value={2}/>
-        <input type="button" value={3}/>
-        <input type="button" value={4}/>
-        <input type="button" value={5}/>
-     </div>
-     <button>Submit</button>
+        <input type="button"value={1} />
+        <input type="button"value={2} />
+        <input type="button"value={3} />
+        <input type="button"value={4} />
+        <input type="button"value={5} />
+      </div>
+
+      <button>Submit</button>
     </div>
   )
 }
